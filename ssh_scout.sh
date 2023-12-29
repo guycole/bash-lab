e610cd07300fc99f44f4c7acbacf924380c3dba0
Guy Cole <guycole@gmail.com>
2023-12-28 17:04:31 -0800

    tweak

--- ssh_scout.sh
+++ ssh_scout.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 # Title: ssh_scout.sh
-# Description: generate a file
+# Description: generate a file to visit all the ip address
 # Development Environment: Ubuntu 18.04.3 LTS (Bionic Beaver)
 # Author: G.S. Cole (guycole at gmail dot com)
 #
 PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 #
-for last_octet in {1..254}
+for last_octet in {2..254}
 do
-  printf "ssh gsc@192.168.1.%3.3d\n" "$last_octet" 
+  printf "ssh -o ConnectTimeout=2 gsc@192.168.1.%d\n" "$last_octet" 
 done
